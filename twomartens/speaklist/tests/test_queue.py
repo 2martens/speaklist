5321a4fb27422f7f1d8ddd9defa20bc72e5ad1bf
Jim Martens <github@2martens.de>
2018-03-29 14:28:17 +0200

    Modified test class to test Queue methods
    Signed-off-by: Jim Martens <github@2martens.de>

--- twomartens/speaklist/tests/test_queue.py
+++ twomartens/speaklist/tests/test_queue.py
@@ -16,63 +16,32 @@
 
 from unittest import TestCase
 
-from twomartens.speaklist.queue import Queue
+from twomartens.speaklist.queue import Queue, FirstSpeakerPriority, FITSoftPriority
 
 
 class TestQueue(TestCase):
-    """Tests the Queue.
-    
-    For now only quotation of women, inter and trans people is taken care of."""
-    def setUp(self):
-        self._queue = Queue()
-        self._speakers = [
-            ("Anton", False),
-            ("Malte", False),
-            ("Jim", False),
-            ("Anna", True),
-            ("Sophia", True),
-            ("Christina", True),
-            ("Anna", True),
-            ("Sophia", True),
-            ("Jim", False),
-            ("Corinna", True),
-            ("Malte", False),
+    """Tests the Queue."""
+    def setUp(self) -> None:
+        """Sets up the test case."""
+        priorities = [
+            FirstSpeakerPriority(),
+            FITSoftPriority()
         ]
-        self._speakersSorted = [
-            "Anna",
-            "Anton",
-            "Sophia",
-            "Malte",
-            "Christina",
-            "Jim",
-            "Anna",
-            "Sophia",
-            "Jim",
-            "Corinna",
-            "Malte",
+        self._queue = Queue(priorities)
+    
+    def test_initialization(self):
+        self.assertEqual(0, len(self._queue))
+        self.assertTrue(self._queue.is_prioritized())
+    
+    def test_prioritize(self):
+        self._queue.prioritize()
+        self.assertTrue(self._queue.is_prioritized())
+    
+    def test_insert(self):
+        new_speaker = [
+            'Speaker 1',
+            'speaker 1',
+            False
         ]
-        
-    def test_add_list(self):
-        # empty list, add list, list is sorted correctly
-        self._queue.add_list(self._speakers)
-        self.assertEqual(self._speakersSorted, self._queue.get_all())
-    
-    def test_get_next(self):
-        # empty list, add list, get first speaker, get second speaker
-        self._queue.add_list(self._speakers)
-        self.assertEqual(self._speakersSorted[0], self._queue.get_next())
-        self.assertEqual(self._speakersSorted[1], self._queue.get_next())
-    
-    def test_get_all(self):
-        # empty list, add list, list is sorted correctly (copy of add_list test)
-        self._queue.add_list(self._speakers)
-        self.assertEqual(self._speakersSorted, self._queue.get_all())
-
-    def test_add(self):
-        # empty list, add list, get sorted list, add new speaker, check that new speaker is in list
-        self._queue.add_list(self._speakers)
-        sorted_speakers = self._queue.get_all()
-        new_speaker = ("Oxford", True)
-        self._queue.add(new_speaker)
-        sorted_speakers.append(new_speaker[0])
-        self.assertEqual(sorted_speakers, self._queue.get_all())
+        self._queue.insert(len(self._queue), new_speaker)
+        self.assertTrue('Speaker 1' in self._queue)
